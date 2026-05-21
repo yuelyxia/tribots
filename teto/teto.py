a8fb0a02b47725c189b9f8c484d5b5e95d91993f
yuelyxia <peachypie6616@gmail.com>
2026-05-21 20:34:46 +0800

    minor changes

--- teto/teto.py
+++ teto/teto.py
@@ -510,7 +510,9 @@ async def mc(ctx, *, to_check: str = None):
                     user_query = {"_id": str(user_id)}
                     trusteduser_profile = trusteduserscol.find_one(user_query)
                     if trusteduser_profile:
-                        description += f"\n{user.mention} `{user.id}` is trusted.\n"
+                        if not(trusteduser_profile["current_staff"] == 0 and trusteduser_profile["staff"] == 0 and \
+                            trusteduser_profile["mm"] == 0 and trusteduser_profile["pilot"] == 0 and trusteduser_profile["trader"] == 0):
+                            description += f"\n{user.mention} `{user.id}` is trusted.\n"
                     else:
                         user_profile = userscol.find_one(user_query)
                         if user_profile:
@@ -589,6 +591,9 @@ async def c(ctx, *, to_check: str = None):
         user_query = {"_id": str(user_id)}
         trusteduser_profile = trusteduserscol.find_one(user_query)
         if trusteduser_profile:
+            if trusteduser_profile["current_staff"] == 0 and trusteduser_profile["staff"] == 0 and \
+                trusteduser_profile["mm"] == 0 and trusteduser_profile["pilot"] == 0 and trusteduser_profile["trader"] == 0:
+                return
             trusted_embed = format_trusteduser_profile(user, trusteduser_profile)
             await ctx.reply("User is trusted.", embed=trusted_embed)
         #
@@ -738,6 +743,9 @@ async def c(ctx, *, to_check: str = None):
             user_query = {"_id": str(user_id)}
             trusteduser_profile = trusteduserscol.find_one(user_query)
             if trusteduser_profile:
+                if trusteduser_profile["current_staff"] == 0 and trusteduser_profile["staff"] == 0 and \
+                    trusteduser_profile["mm"] == 0 and trusteduser_profile["pilot"] == 0 and trusteduser_profile["trader"] == 0:
+                    return
                 trusted_embed = format_trusteduser_profile(user, trusteduser_profile)
                 await ctx.reply("User is trusted.", embed=trusted_embed)
             else:
